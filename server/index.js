46dcdeda12a2f3dc9854da301b263f05f766aa71
gourav <you@example.com>
2021-03-28 14:19:04 +0530

    'working'

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ import createreview from './routes/reviews/createreviews.js';
 import myreviews from './routes/reviews/myreviews.js';
 import signup from './routes/auth/signup.js';
 import login from './routes/auth/login.js';
-
  
 const app = express();
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 
 app.use('/fetchreviews', getreviews);
